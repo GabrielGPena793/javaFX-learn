a8241c7a3b7987bb2702ae48ca53b81c608599f6
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-12 20:24:22 -0300

    Using TextField and Label (app for calculate sum)

--- src/main/java/com/uldemy/gui/ViewController.java
+++ src/main/java/com/uldemy/gui/ViewController.java
@@ -4,16 +4,38 @@ import com.uldemy.gui.util.Alerts;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Locale;
 
 public class ViewController {
+    @FXML
+    private TextField txtNumber1;
 
     @FXML
-    private Button btTest;
+    private TextField txtNumber2;
+
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button btSum;
 
     //chama a função quando clicar no botão
     @FXML
-    public void onBtTestAction(){
-        Alerts.showAlert("Alert title", null, "Hello", Alert.AlertType.ERROR);
+    public void onBtSumAction(){
+        try {
+            Locale.setDefault(Locale.US);
+            double number1 = Double.parseDouble(txtNumber1.getText());
+            double number2 = Double.parseDouble(txtNumber2.getText());
+            double sum = number1 + number2;
+
+            labelResult.setText(String.format("%.2f", sum));
+        }
+        catch (NumberFormatException e){
+            Alerts.showAlert("Error", "Parse error", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 
 }
